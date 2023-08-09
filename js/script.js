07d02ab6243b9fb11b49b488df4a9946db9a7fbb
Piotr Węgłowski <weglowskipiotr@gmail.com>
2023-08-09 19:37:31 +0200

    Add whole function changing active article

--- js/script.js
+++ js/script.js
@@ -1,19 +1,40 @@
 {
   ("use strict");
   const titleClickHandler = function (event) {
+    event.preventDefault();
+
     console.log("Link was clicked");
     console.log(event);
-    /* remove class 'active' from all article links  */
 
+    /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll(".titles a.active");
+
+    for (let activeLink of activeLinks) {
+      activeLink.classList.remove("active");
+    }
     /* add class 'active' to the clicked link */
+    const clickedElement = this;
+    console.log("clickedElement:", clickedElement);
+
+    clickedElement.classList.add("active");
 
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll("article.active");
+
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove("active");
+    }
 
     /* get 'href' attribute from the clicked link */
+    const clickedArticleHref = clickedElement.getAttribute("href");
+    console.log(clickedArticleHref);
 
     /* find the correct article using the selector (value of 'href' attribute) */
+    const clickedArticle = document.querySelector(clickedArticleHref);
+    console.log(clickedArticle);
 
     /* add class 'active' to the correct article */
+    clickedArticle.classList.add("active");
   };
 
   const links = document.querySelectorAll(".titles a");
@@ -21,9 +42,4 @@
   for (let link of links) {
     link.addEventListener("click", titleClickHandler);
   }
-
-  //   document.getElementById("test-button").addEventListener("click", function () {
-  //     const links = document.querySelectorAll(".titles a");
-  //     console.log("links", links);
-  //   });
 }
